2ddbaa5b0265ead6f62e600f05c9cd7a62280bc4
SINGsyung <make75682@gmail.com>
2022-11-03 15:08:25 +0800

    [fix] Judgment question dragged to sub-item

--- src/components/Drag/DragArea.js
+++ src/components/Drag/DragArea.js
@@ -5,38 +5,62 @@ const DragArea = () => {
   const [data, setData] = useState([]);
   const [draged, setDraged] = useState(null);
 
-  const clearOverItem =(overItem)=>{
-    if (!overItem) return;
-    overItem.classList.remove("before");
-    overItem.classList.remove("after");
-    overItem = null;
-  }
+  const clearOverItem = (e) => {
+    if (e.target.className.includes("item")){
+      const overItem = e.target
+      overItem.classList.remove('before');
+      overItem.classList.remove('after');
+      return
+    }
+    if(e.target.offsetParent.className.includes("item")){
+      const overItem = e.target.offsetParent
+      overItem.classList.remove('before');
+      overItem.classList.remove('after');
+      return
+    }
+    
+  };
   const dragStartHandler = (e) => {
-    console.log('dragStart');
+    // console.log('dragStart');
     e.target.classList.add('draged');
     setDraged(e.target);
   };
   const dragEndHandler = (e) => {
-    console.log('dragEnd');
+    // console.log('dragEnd');
     e.target.classList.remove('draged');
     setDraged(null);
   };
   const dragOverHandler = (e) => {
-    const overItem = e.target
-    if (draged === overItem || !overItem.className.includes("item")) return;
-    console.log("offset",e.nativeEvent.offsetX);
-    console.log("width",overItem.clientWidth);
-    if(e.nativeEvent.offsetX > overItem.clientWidth/2){
-      clearOverItem(overItem)
-      overItem.classList.add("after")
-    }else{
-      clearOverItem(overItem)
-      overItem.classList.add("before")
-    }    
+    // console.log(e);
+    let overItem = e.target;
+    if (draged === overItem) return;
+    if (overItem.className.includes('item')) {
+      if (e.nativeEvent.offsetX > overItem.clientWidth / 2) {
+        clearOverItem(e);
+        overItem.classList.add('after');
+      } else {
+        clearOverItem(e);
+        overItem.classList.add('before');
+      }
+    } else if (overItem.offsetParent.className.includes('item')) {
+      overItem = overItem.offsetParent;
+      if (e.nativeEvent.layerX > overItem.clientWidth / 2) {
+        clearOverItem(e);
+        overItem.classList.add('after');
+      } else {
+        clearOverItem(e);
+        overItem.classList.add('before');
+      }
+    }
+    // console.log('offset', e.nativeEvent.offsetX);
+    // console.log('width', overItem.clientWidth);
   };
-  const dragLeaveHandler = (e)=>{
-    clearOverItem(e.target)
-  }
+  const dragLeaveHandler = (e) => {
+    clearOverItem(e);
+  };
+  const dropHandler = (e) => {
+    clearOverItem(e);
+  };
 
   useEffect(() => {
     const array = [];
@@ -54,11 +78,15 @@ const DragArea = () => {
       onDragEnd={dragEndHandler}
       onDragOver={dragOverHandler}
       onDragLeave={dragLeaveHandler}
+      onDrop={dropHandler}
     >
       {data.map((i) => {
         return (
           <div className="item" key={i} draggable="true">
             <h1>{i}</h1>
+            <div>
+              <h2>test</h2>
+            </div>
           </div>
         );
       })}
